8568ce1b5351afd2c4f08a44b45c9c7ffe41fbf1
deadbird11 <eddiejr@hatfieldfamily.com>
2020-08-21 12:36:24 -0400

    added vector generation to Random.h

--- onewknd/src/util/Random.h
+++ onewknd/src/util/Random.h
@@ -3,6 +3,7 @@
 #include <random>
 #include <stdio.h>
 #include <typeinfo>
+#include <glm/glm.hpp>
 
 // I want to expand on this eventually
 class Random
@@ -12,23 +13,27 @@ public:
 	static std::mt19937 s_Engine;
 
 	template<typename T>
-	static T GetRandomInRange(T a, T b)
+	inline static T GetRandomInRange(T a, T b)
 	{
 		static_assert(false, 
 			std::sprintf("Random does not support this data type: %v", typeid(T).name));
 	}
 
 	template<>
-	static float GetRandomInRange(float a, float b)
+	inline static float GetRandomInRange(float a, float b)
 	{
 		std::uniform_real_distribution<float> distribution(a, b);
 		return distribution(s_Engine);
 	}
 
 	template<>
-	static int GetRandomInRange(int a, int b)
+	inline static int GetRandomInRange(int a, int b)
 	{
 		std::uniform_int_distribution<int> distribution(a, b);
 		return distribution(s_Engine);
+	}
+
+	inline static glm::vec3 GetRandomVecInRange(float min, float max) {
+		return glm::vec3(GetRandomInRange<float>(min, max), GetRandomInRange<float>(min, max), GetRandomInRange<float>(min, max));
 	}
 };
